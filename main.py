99c51768fdf6d9fead402c0a6fb43736a211bc4a
Mayankpratapsingh022 <mayankpratapsingh022@gmail.com>
2025-01-28 21:34:51 +0530

    coded feature_engineering

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import os
 
 os.system("python src/data_ingestion.py")
+print("Data ingestion run")
+os.system("python src/data_preprocessing.py")
+print("data_preprocessing run")
+
+os.system("python src/feature_engineering.py")
+print("feature_engineering run")
